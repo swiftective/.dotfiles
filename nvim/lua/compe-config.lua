4dba384633650494ee4d414fb83403404ec00546
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-09-30 22:02:14 +0530

    Edit compe-config

--- nvim/lua/compe-config.lua
+++ nvim/lua/compe-config.lua
@@ -17,12 +17,12 @@ cmp.setup({
     end,
   },
   mapping = {
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    -- ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
-    ['<C-j>'] = cmp.mapping.confirm({ select = true }),
+    ['<CR>'] = cmp.mapping.confirm({ select = false }),
   },
   sources = {
     -- For ultisnips user.
