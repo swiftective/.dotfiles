583f19e5e69243f426f6e8eba0c19773148d1c99
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-10-03 01:11:08 +0530

    Edit defaults.zsh

--- zsh/scripts/defaults.zsh
+++ zsh/scripts/defaults.zsh
@@ -21,8 +21,7 @@ bindkey -M vicmd e vi-backward-word
 # Edit line in vim with ctrl-e:
 autoload edit-command-line; zle -N edit-command-line
 bindkey '^e' edit-command-line
-
-
+bindkey -M vicmd '^e' edit-command-line
 
 # Change cursor shape for different vi modes.
 function zle-keymap-select {
