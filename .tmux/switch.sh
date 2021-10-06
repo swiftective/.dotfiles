26768f1f43332272746d6176c66812683d3d112e
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-10-06 07:42:50 +0530

    Edit .tmux.conf

--- .tmux/switch.sh
+++ .tmux/switch.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # switch tmux sessions
-if [[ $@ == tss ]]; then
+if [[ $1 == tss ]]; then
   tmux ls -F '#S' | fzf-tmux -p --reverse | xargs -I {} tmux switchc -t '{}'
 fi
 
 # switch tmux windows
-if [[ $@ == tsw ]]; then
+if [[ $1 == tsw ]]; then
   tmux lsw | fzf-tmux -p --reverse | awk -F ':' '{print $1}' | xargs -I {} tmux selectw -t '{}'
 fi
 
 # switch all tmux windows
-if [[ $@ == tsW ]]; then
+if [[ $1 == tsW ]]; then
   tmux list-windows -a -F '#S:#I: #W' | fzf-tmux -p --reverse | sed 's/\ /\\\ /g' | awk -F ':' '{print $1":"$2}' | xargs tmux switchc -t
 fi
