fba955cb335d8ec82f1ef27933de2981fbf1359d
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-10-06 22:09:08 +0530

    Edit var names

--- .tmux/switch.sh
+++ .tmux/switch.sh
@@ -2,15 +2,15 @@
 
 # switch tmux sessions
 if [[ $1 == tss ]]; then
-  tmux ls -F '#S' | fzf-tmux -p --reverse | xargs -I {} tmux switchc -t '{}'
+  tmux ls -F '#S' | fzf-tmux -p --reverse | xargs -r -I {} tmux switchc -t '{}'
 fi
 
 # switch tmux windows
-if [[ $1 == tsW ]]; then
-  tmux lsw | fzf-tmux -p --reverse | awk -F ':' '{print $1}' | xargs -I {} tmux selectw -t '{}'
+if [[ $1 == tsw ]]; then
+  tmux lsw | fzf-tmux -p --reverse | awk -F ':' '{print $1}' | xargs -r -I {} tmux selectw -t '{}'
 fi
 
 # switch all tmux windows
-if [[ $1 == tsw ]]; then
-  tmux list-windows -a -F '#S:#I: #W' | fzf-tmux -p --reverse | sed 's/\ /\\\ /g' | awk -F ':' '{print $1":"$2}' | xargs tmux switchc -t
+if [[ $1 == tsW ]]; then
+  tmux list-windows -a -F '#S:#I: #W' | fzf-tmux -p --reverse | sed 's/\ /\\\ /g' | awk -F ':' '{print $1":"$2}' | xargs -r tmux switchc -t
 fi
