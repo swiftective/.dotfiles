1161a6452240307128aadee838fc5a951c643031
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-10-13 12:45:19 +0530

    Edit remaps for barbar-config

--- nvim/lua/barbar-config.lua
+++ nvim/lua/barbar-config.lua
@@ -24,7 +24,7 @@ map('n', '<Space>bb', ':BufferOrderByBufferNumber<CR>', opts)
 map('n', '<Space>bd', ':BufferOrderByDirectory<CR>', opts)
 map('n', '<Space>bl', ':BufferOrderByLanguage<CR>', opts)
 -- pin buffer
-map('n', '<leader>bi', ':BufferOrderByLanguage<CR>', opts)
+map('n', '<leader>bi', ':BufferPin<CR>', opts)
 
 
 -- Set barbar's options
