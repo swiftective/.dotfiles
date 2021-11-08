cdf8d0125f65df336949211e26676424048da5dc
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-11-08 14:25:39 +0530

    chore(maintenance): Update locale for .tmux.conf

--- zsh/scripts/env.zsh
+++ zsh/scripts/env.zsh
@@ -2,7 +2,7 @@
 export XDG_DATA_HOME=${XDG_DATA_HOME:="$HOME/.local/share"}
 export EDITOR='nvim'
 export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
+export LANG="en_US.UTF-8"
 export VISUAL='nvim'
 export MANPAGER="sh -c 'col -bx | bat --theme TwoDark -l man -p'"
 
