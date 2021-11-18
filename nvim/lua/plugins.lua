ed30796321d331cceff6b5ab6b79e4363afd39a0
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-11-18 16:59:40 +0530

    Edit plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -3,20 +3,6 @@ local disabled_built_ins = {
   "netrwPlugin",
   "netrwSettings",
   "netrwFileHandlers",
-  "gzip",
-  "zip",
-  "zipPlugin",
-  "tar",
-  "tarPlugin",
-  "getscript",
-  "getscriptPlugin",
-  "vimball",
-  "vimballPlugin",
-  "2html_plugin",
-  "logipat",
-  "rrhelper",
-  "spellfile_plugin",
-  "matchit",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
