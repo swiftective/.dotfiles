2fed1d6fc30f7388ff60a94d16c470ca3de4b28d
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-11-18 07:42:13 +0530

    Edit nvim-config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -60,7 +60,6 @@ return packer.startup(function()
       vim.g.onedark_transparent_background = true
       vim.g.onedark_hide_ending_tildes = true
       vim.g.onedark_style = "deep"
-      vim.cmd [[colorscheme onedark]]
     end,
   }
   use {
