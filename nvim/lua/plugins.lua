2902d4130f508dac2976b9006c71dc0538f20c97
Sashane RV <detectivesherlockholmes4u@gmail.com>
2021-11-18 07:07:23 +0530

    Edit plugins.lua

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -23,7 +23,7 @@ for _, plugin in pairs(disabled_built_ins) do
   vim.g["loaded_" .. plugin] = 1
 end
 
-vim.opt.shell = "/bin/bash"
+vim.opt.shell = "/bin/dash"
 
 local packer = require "packer"
 
