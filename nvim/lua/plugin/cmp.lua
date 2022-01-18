c7a88ce5338a60d7e82ba85045216f9c629b8363
Sashane RV <detectivesherlockholmes4u@gmail.com>
2022-01-18 16:27:57 +0530

    Neovim: Change LSPKIND icons to default

--- nvim/lua/plugin/cmp.lua
+++ nvim/lua/plugin/cmp.lua
@@ -86,7 +86,7 @@ cmp.setup {
   },
   formatting = {
     format = function(entry, vim_item)
-      vim_item.kind = lspkind.presets.codicons[vim_item.kind] .. " "
+      vim_item.kind = lspkind.presets.default[vim_item.kind] .. " "
       local menu = source_mapping[entry.source.name]
       if entry.source.name == "cmp_tabnine" then
         if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
