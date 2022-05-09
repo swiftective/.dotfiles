9fd296a1acab0c899779a8422aeea9b0f18b0216
swiftective <87100566+swiftective@users.noreply.github.com>
2022-05-09 22:06:24 +0530

    Update config.el

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Sashane RV"
+(setq user-full-name "swiftective"
       user-mail-address "nil")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
