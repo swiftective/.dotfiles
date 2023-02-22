06350299bcda466f71c42ff72bf3e677fd2f3ff4
Sashane RV <detectivesherlockholmes4u@gmail.com>
2023-02-22 10:46:56 +0530

    Neovim: Treesitter config update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -9,7 +9,6 @@ return {
       require("nvim-treesitter.configs").setup {
         rainbow = {
           enable = true,
-          disable = { "html" },
           extended_mode = true,
           max_file_lines = nil,
         },
