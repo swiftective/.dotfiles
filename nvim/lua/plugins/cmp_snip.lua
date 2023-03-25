4eb0516ffb0c764f4cacec9ce726beb0ab3c21a2
Sashane RV <detectivesherlockholmes4u@gmail.com>
2023-03-25 12:17:16 +0530

    Random Update Tmux: Add keybinding Neovim: Remove cmdline from cmp source

--- nvim/lua/plugins/cmp_snip.lua
+++ nvim/lua/plugins/cmp_snip.lua
@@ -144,7 +144,6 @@ return {
           { name = "nvim_lua" },
           { name = "git" },
           { name = "path" },
-          { name = "cmdline" },
           {
             name = "buffer",
             option = {
@@ -192,7 +191,6 @@ return {
     dependencies = {
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-cmdline" },
       { "hrsh7th/cmp-path" },
       { "hrsh7th/cmp-nvim-lua" },
       {
