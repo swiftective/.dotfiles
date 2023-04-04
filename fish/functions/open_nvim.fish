3da7cb4c1f05f33a812fcee53f8cc4bd94eed099
Sashane RV <detectivesherlockholmes4u@gmail.com>
2023-04-04 22:08:17 +0530

    Fish: Edit open_nvim function

--- fish/functions/open_nvim.fish
+++ fish/functions/open_nvim.fish
@@ -1,4 +1,4 @@
 function open_nvim
   set -l preview 'bat --style=numbers --color=always --line-range :500 {}'
-  fd -t f | sk --preview="$preview" --prompt="🔍" | xargs -r nvim
+  nvim (fd -t f | sk --preview="$preview" --prompt="🔍")
 end
