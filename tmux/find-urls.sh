35be34ee6703f883f72fa25044d65c64d61484e4
Sashane RV <detectivesherlockholmes4u@gmail.com>
2023-05-13 18:59:07 +0530

    tmux: add find-urls script to copy urls

---
+++ tmux/find-urls.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+url_regex='https?://[^\s"]+'
+
+url=$(tmux capture-pane -pS -10000 | rg -oP "$url_regex" | sk)
+
+if [[ "$url" == "" ]]; then
+  exit 0
+fi
+
+echo $url | nohup xclip -loops 0 -sel clipboard
+
+killall xclip
