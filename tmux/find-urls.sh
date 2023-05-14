e1f8b536100e05184fbe7ea67b4d7571c02abab3
Sashane RV <detectivesherlockholmes4u@gmail.com>
2023-05-15 00:08:53 +0530

    tmux: find-urls.sh add uniq to piping for uniq urls add quotes and apostrophe

--- tmux/find-urls.sh
+++ tmux/find-urls.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-url_regex='https?://[^\s"]+'
+url_regex="https?://[^\s\"']+"
 
-url=$(tmux capture-pane -pS -10000 | rg -oP "$url_regex" | sk)
+url=$(tmux capture-pane -pS -10000 | rg -oP "$url_regex" | uniq | sk)
 
 if [[ "$url" == "" ]]; then
   exit 0
