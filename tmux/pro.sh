50543cca5fff729a7844b8e9e070105fa3121d25
Sashane RV <detectivesherlockholmes4u@gmail.com>
2023-07-28 19:30:53 +0530

    tmux: port pro.sh to dash

--- tmux/pro.sh
+++ tmux/pro.sh
@@ -1,28 +1,20 @@
-#!/bin/bash
+#!/bin/dash
 
-if [[ ! -d ~/projects ]]; then
+if [ ! -d ~/projects ]; then
   exit 1
 fi
 
-cd ~/projects
+dir=$(fd -t d -H -g .git --base-directory ~/projects | xargs dirname | sk)
 
-dir=$(for i in $(fd -t d -H)
-do
-  if [[ -d "$i/.git" ]]; then
-    echo $i
-  fi
-done | sk)
-
-if [[ ! $dir ]]; then
+if [ "$dir" = "" ]; then
   exit 1
 fi
 
-session=$(echo projects/$dir | sed 's/\/$//' | sed 's/\//·/g' | sed 's/\./_/g')
-
+session=$(echo projects/$dir | sed 's/\/$//' | sed 's/\//·/g' | sed 's/\./·/g')
 
 if tmux ls | grep -q $session; then
   tmux switch-client -t $session
 else
-  tmux rename-session $session
-  fish -C "cd ~/projects/$dir"
+  tmux new-session -d -s $session -c "$HOME/projects/$dir"
+  tmux switch-client -t $session
 fi
