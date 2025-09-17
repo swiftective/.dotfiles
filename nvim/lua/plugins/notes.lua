89e3ba1be7e4c1fbfdca43f861ca51d957968c53
swiftective <detectivesherlockholmes4u@gmail.com>
2025-09-17 16:54:50 +0530

    nvim: update

--- nvim/lua/plugins/notes.lua
+++ nvim/lua/plugins/notes.lua
@@ -1,7 +1,8 @@
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
-    event = "VeryLazy",
+    cmd = "RenderMarkdown",
+    -- event = "VeryLazy",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     opts = {
 
